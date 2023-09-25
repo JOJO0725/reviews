c71d871ef5507d96dd357424ffc40090697ace18
JOJO0725 <bleach810725@gmail.com>
2023-09-25 16:52:41 +0800

    ReallyFinalFile

--- review.py
+++ review.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+new = []
 with open('reviews.txt','r') as r:
     for line in r:
         data.append(line)
@@ -11,6 +12,9 @@ sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
     avg = sum_len / len(data)
+    if len(d) < 100:
+        new.append(d)
+print('少於100字的留言共',len(new),'筆')
 print('留言平均的長度:',avg)
 
 
